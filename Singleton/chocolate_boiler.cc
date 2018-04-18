df487669b615e741acbecc75ca27f9e6335e61b3
Martin Nobis <martin.nobis@gmail.com>
2018-04-18 12:30:50 +1000

    Completed Singleton.

---
+++ Singleton/chocolate_boiler.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+
+#include "chocolate_boiler.h"
+
+ChocolateBoiler& ChocolateBoiler::GetInstance() {
+    static ChocolateBoiler instance;
+    return instance;
+}
+
+void ChocolateBoiler::Fill(void) {
+    if (IsEmpty()) {
+        std::cout << "Filled!" << std::endl;
+        empty_ = false;
+        boiled_ = false;
+    }
+}
+
+void ChocolateBoiler::Drain(void) {
+    if (!IsEmpty() && IsBoiled()) {
+        std::cout << "Drained!" << std::endl;
+        empty_ = true;
+    }
+}
+
+void ChocolateBoiler::Boil(void) {
+    if (!IsEmpty() && !IsBoiled()) {
+        std::cout << "Boiled!" << std::endl;
+        boiled_ = true;
+    }
+}
+
+bool ChocolateBoiler::IsEmpty(void) {
+    return empty_;
+}
+
+bool ChocolateBoiler::IsBoiled(void) {
+    return boiled_;
+}
---
+++ Singleton/main.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+#include "chocolate_boiler.h"
+
+int main(void) {
+    ChocolateBoiler& instance = ChocolateBoiler::GetInstance();
+    instance.Fill();
+    instance.Boil();
+    instance.Drain();
+    instance.Drain();
+    instance.Boil();
+    instance = ChocolateBoiler::GetInstance();
+    instance.Drain();
+
+    return 0;
+}
